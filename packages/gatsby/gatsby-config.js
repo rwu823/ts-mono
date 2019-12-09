e0b986b30b31439c4ce4a88cfdbadf0dbe934c9d
Rocky Wu <rocky@17.media>
2019-12-09 09:45:07 +0800

    Upgrade pkgs

--- packages/gatsby/gatsby-config.js
+++ packages/gatsby/gatsby-config.js
@@ -47,16 +47,16 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-typescript',
+      resolve: `gatsby-plugin-typescript`,
       options: {
         allowNamespaces: true,
       },
     },
-    'gatsby-plugin-mdx',
+    `gatsby-plugin-mdx`,
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: `gatsby-source-filesystem`,
       options: {
-        name: 'posts',
+        name: `posts`,
         path: `${__dirname}/src/posts`,
       },
     },
