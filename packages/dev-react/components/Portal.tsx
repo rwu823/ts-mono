c788943c46294433c2d3fd12daa7b13d558199d1
Rocky Wu <rocky@17.media>
2020-01-06 10:51:45 +0800

    fix Portal

--- packages/dev-react/components/Portal.tsx
+++ packages/dev-react/components/Portal.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { createPortal } from 'react-dom'
 
 const globalThis = (1, eval)('this') // eslint-disable-line no-eval
-const isDom = 'document' in globalThis && 'body' in globalThis.document
 
 type Props = {
   element?: HTMLElement | null
@@ -10,7 +9,7 @@ type Props = {
 
 const Portal: React.FC<Props> = ({
   children,
-  element = isDom ? document.body : null,
-}) => (isDom ? createPortal(children, element!) : <>{children}</>)
+  element = globalThis.document?.body ?? null,
+}) => (element ? createPortal(children, element) : <>{children}</>)
 
 export default Portal
