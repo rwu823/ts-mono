cc3f0538a636633ac7c66565316cfe9381a1227c
Rocky Wu <rocky@17.media>
2020-02-27 09:26:29 +0800

    get prevState

--- packages/dev-react/hooks/useObjectState.ts
+++ packages/dev-react/hooks/useObjectState.ts
@@ -1,17 +1,43 @@
 import { useState, useCallback } from 'react'
-export const useObjectState = <T extends object>(
-  initState: T,
-): [T, (newState: Partial<T>) => void] => {
+
+export const useObjectState = <T extends object>(initState: T) => {
   const [state, setState] = useState(initState)
 
-  const set = useCallback((newState: Partial<T>) => {
-    setState(prevState => ({
-      ...prevState,
-      ...newState,
-    }))
+  type SetPrevState = (prev: Partial<T>) => Partial<T>
+  type NewState = Partial<T>
+  type SetStateFunc = {
+    (newState: NewState): void
+    (setFunc: SetPrevState): void
+  }
+
+  const set = useCallback<SetStateFunc>((newState: SetPrevState | NewState) => {
+    if (typeof newState === 'function') {
+      setState(prevState => {
+        return {
+          ...prevState,
+          ...newState(prevState),
+        }
+      })
+    } else {
+      setState(prevState => ({
+        ...prevState,
+        ...newState,
+      }))
+    }
   }, [])
 
-  return [state, set]
+  const getState = useCallback(
+    () =>
+      new Promise(resolve => {
+        set(prev => {
+          resolve(prev)
+          return {}
+        })
+      }),
+    [set],
+  )
+
+  return [state, set, getState] as [T, SetStateFunc, () => Promise<T>]
 }
 
 export default useObjectState
