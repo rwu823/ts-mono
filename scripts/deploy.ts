3e4923d4bf468592fc6356e4924d2fa2eb7c8453
rocky <rwu823@gmail.com>
2022-08-07 11:35:48 +0800

    use pnpm instead of dpeloy.ts

--- scripts/deploy.ts
+++
@@ -1,8 +0,0 @@
-import fg from 'fast-glob'
-import sh from 'sh-exec'
-
-const pkgs = fg.sync('./*', { onlyDirectories: true, cwd: 'packages' })
-
-sh`
-${pkgs.map((pkg) => `yarn workspace @ts-mono/${pkg} deploy`).join('\n')}
-`
