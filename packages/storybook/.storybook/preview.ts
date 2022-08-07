c05098fb936810821bf4c090f4031440b937bbe1
rocky <rwu823@gmail.com>
2022-08-08 04:58:50 +0800

    remove storybook

--- packages/storybook/.storybook/preview.ts
+++
@@ -1,5 +0,0 @@
-import { addDecorator } from '@storybook/react' // <- or your view layer
-
-export const parameters = {
-  actions: { argTypesRegex: '^on.*', handles: ['mouseover', 'click'] },
-}
--- packages/storybook/.storybook/types.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '*.mdx' {
-  const mdx: string
-  export = mdx
-}
