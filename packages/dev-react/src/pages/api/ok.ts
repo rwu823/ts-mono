f3b5aaf700b284673bbb52e50855b13adc07e84b
rocky <rwu823@gmail.com>
2023-09-24 20:17:06 +0800

    fix: remove page

--- packages/dev-react/src/pages/api/ok.ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiHandler } from 'next'
-
-export default ((_req, res) => {
-  res.send({
-    date: process.env.DATE,
-    sha: process.env.VERCEL_GIT_COMMIT_SHA ?? '-',
-  })
-}) as NextApiHandler
