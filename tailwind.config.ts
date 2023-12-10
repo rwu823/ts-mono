c26d83ba1bc81b6bb0fac9b7457ba3fc9efc8966
rocky <rwu823@gmail.com>
2023-12-10 10:12:35 +0800

    fix: tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,49 @@
+import path from 'node:path'
+
+import type { Config } from 'tailwindcss'
+import plugin from 'tailwindcss/plugin.js'
+
+export default {
+  content: [path.resolve('src/**/*.tsx')],
+  theme: {
+    extend: {},
+  },
+  blocklist: [],
+  plugins: [
+    plugin(({ addUtilities, matchUtilities, theme, addVariant }) => {
+      addVariant('input-number-spin', [
+        '&::-webkit-outer-spin-button',
+        '&::-webkit-inner-spin-button',
+      ])
+
+      addVariant('scrollbar', ['&::-webkit-scrollbar'])
+      addVariant('scrollbar-track', ['&::-webkit-scrollbar-track'])
+      addVariant('scrollbar-thumb', ['&::-webkit-scrollbar-thumb'])
+
+      addVariant('not-last-child', '&:not(:last-child)')
+      addVariant('child', '&>*')
+
+      matchUtilities(
+        {
+          appearance: (value) => {
+            switch (value) {
+              case 'textfield': {
+                return {
+                  appearance: value,
+                  '-moz-appearance': value,
+                }
+              }
+            }
+
+            return null
+          },
+        },
+        {
+          values: {
+            textfield: 'textfield',
+          },
+        },
+      )
+    }),
+  ],
+} satisfies Config
